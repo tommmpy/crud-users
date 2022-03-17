ec33034fc7e9795508898979cb84c06ae42a06da
tommmpy <alveztomas2004@gmail.com>
2022-03-17 02:04:40 -0300

    CRUD de Users

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "/users", to: "users#index"
+  get "/users/new", to: "users#new"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "/users/:id", to: "users#update"
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#delete", as: "delete_user"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.destroy_all
+
+user1 = User.create(name: "Tomas", info: "Varon de 18 años obsesionado por la programacion", mail: "alveztomas2004@gmail.com")
+
+user2 = User.create(name: "Guillermo", info: "Hombre de 45 años que quiere mucho a sus hijos", mail: "galvez76@gmail.com")
